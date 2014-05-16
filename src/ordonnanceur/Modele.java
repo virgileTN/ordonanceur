18e79f1deefa1092344403192f176b184cfc30e2
kickass <virgile.dauge@telecomnancy.net>
2014-05-16 20:36:09 +0200

    Ajout modele (vraiment ce coup-ci)

---
+++ src/ordonnanceur/Modele.java
@@ -0,0 +1,45 @@
+package ordonnanceur;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.List;
+
+public class Modele {
+	TaskList listeTache;
+	Ordonnanceur ordo;
+	public Modele(){
+		
+	}
+	public Modele(int NbCoeurs, String typOrd, String cheminFichier, int quantum) throws FileNotFoundException{
+		InputStream is = new FileInputStream(cheminFichier);
+		listeTache = new TaskList(is);
+		switch(typOrd){
+		//mode    First In First out
+		case "FIFO" :
+			ordo = new FifoOrdonnanceur(NbCoeurs);
+			break;
+		//mode Round Robin
+		case "RR" :
+			ordo = new RrOrdonnanceur(NbCoeurs,quantum);
+			break;
+		//mode Priority
+		case "PR" :
+			ordo = new PrOrdonnanceur(NbCoeurs);
+			break;
+		//mode shortest remaining Time
+		case "SRT" :
+			ordo = new SrtOrdonnanceur(NbCoeurs);
+			break;
+		}
+	}
+	public List<Coeur> getResult(){
+		return ordo.getResult(listeTache);
+	}
+	public void addTask(Task tache){
+		listeTache.addTask(tache);
+	}
+	public void effectuer(int temps){
+		ordo.effectuer(temps); 
+	}
+}
