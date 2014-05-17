70202c7f984df5eda6e9c8122590da509116c0d4
kompass <kirchner.nic@gmail.com>
2014-05-17 16:40:00 +0200

    Petit nettoyage

--- src/ordonnanceur/AbstractOrdonnanceur.java
+++ src/ordonnanceur/AbstractOrdonnanceur.java
@@ -1,10 +1,6 @@
 package ordonnanceur;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 
 public abstract class AbstractOrdonnanceur implements Ordonnanceur{
