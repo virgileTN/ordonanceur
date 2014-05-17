574b0e542009dd267a61505096d9f2fd1f18af74
kompass <kirchner.nic@gmail.com>
2014-05-17 16:20:00 +0200

    Ajout PROrdonnanceur

---
+++ src/ordonnanceur/PROrdonnaceur.java
@@ -0,0 +1,25 @@
+package ordonnanceur;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class PROrdonnaceur extends AbstractOrdonnanceur {
+
+	public PROrdonnaceur(int nbCoeur) {
+		super(nbCoeur);
+	}
+
+	@Override
+	protected Task nextTask(List<Task> pendingList) {
+		return Collections.min(pendingList, new Comparator<Task>() {
+			public int compare(Task t1, Task t2) {
+				if(t1.getPrio() == t2.getPrio())
+					return Integer.compare(t1.getNum(), t2.getNum());
+				else
+					return Integer.compare(t1.getPrio(), t2.getPrio());
+			}
+		});
+	}
+
+}
